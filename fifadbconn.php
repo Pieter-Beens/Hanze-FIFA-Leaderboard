0dfb4f640c5ebeee8ad1a6a2f56bbb3f34b02e09
Dennis <dennissmid_97@hotmail.nl>
2018-10-19 21:45:26 +0200

    usbwebserver fix

--- fifadbconn.php
+++ fifadbconn.php
@@ -5,11 +5,23 @@ $dbuser = "root";
 $dbpass = ""; //alleen als er een wachtwoord is toegepast
 $dbname = "fifa"; //naam van de database
 $db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-// Test of de verbinding werkt!
+
+// Test of de verbinding werkt! (Voor die smerige XAMP)
 if (mysqli_connect_errno()) {
-die("Failure connecting to the database: " .
-mysqli_connect_error() . " (" .
-mysqli_connect_errno() . ")" );
+	// Als dat niet werkt probeert hij mijn settings zodat mijn database ook werkt niet aanpassen A U B -Dennis 
+	
+	// Database connectie met localhost
+	$dbpass = "usbw"; // zet wachtwoord voor mijn server (is verplicht bij usbwebserver)
+	$db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	// Test of de verbinding werkt! (Voor die geweldige usbwebserver 8.6.2)
+	if (mysqli_connect_errno()) {
+		die("Failure connecting to the database: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")" );
+	};
+	
 };
+
+
+
+
 
 ?>
