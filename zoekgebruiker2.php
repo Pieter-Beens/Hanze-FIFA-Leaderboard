4b2253d3073826089dd69a046b28fdb8b3b717c9
Pieter-Beens <43377042+Pieter-Beens@users.noreply.github.com>
2018-11-05 13:21:13 +0100

    Update zoekgebruiker2.php

--- zoekgebruiker2.php
+++ zoekgebruiker2.php
@@ -1,4 +1,5 @@
 <html>
+  <head>  <?php include('header.php'); ?> </head>
 <body>
 <form action = "resultaataanpas.php" method = "post">
 Vul de username van de speler in: <input type="text" name = "username"><br>
