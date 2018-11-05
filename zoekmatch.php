6a3b61b0eda11664c665e66c0bf44f13669b59f4
Pieter-Beens <43377042+Pieter-Beens@users.noreply.github.com>
2018-11-05 13:20:11 +0100

    Update zoekmatch.php

--- zoekmatch.php
+++ zoekmatch.php
@@ -1,6 +1,8 @@
 <html>
+ <head>
+   <?php include('header.php'); ?>
+ </head>
 <body>
- <?php include('header.php'); ?>
 <form action = "resultaatmatch.php" method = "post">
 Vul de ID van de home player in: <input type="text" name = "players"><br>
 <input type = "submit">
